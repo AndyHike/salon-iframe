e390fa7955d1e32f99bf05fbb610a7727d913eb0
Your Name <example@example.com>
2026-03-25 16:32:02 +0100

    тт

--- scripts/test-api.ts
+++
@@ -1,8 +0,0 @@
-import { loadBeautySalonHome } from './cms/loaders/loadBeautySalonHome';
-
-async function main() {
-  const data = await loadBeautySalonHome('localhost:3000');
-  console.log(JSON.stringify(data?.servicesItems?.[0], null, 2));
-}
-
-main().catch(console.error);
